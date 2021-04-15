07e112d42710d7b1c4ed7862d67ae3e02b03bbdd
ECSchmitt <ericcheschmitt@gmail.com>
2021-04-15 13:12:05 +0200

    Added funtion to change rownames of a matrix within diplyr pipe

---
+++ R/chg_rownames.R
@@ -0,0 +1,18 @@
+#' Changes rownames of a matrix
+#'
+#' Function takes a vector containing names and a matrix and exchanges
+#' the rownames of the matrix with the ones provided in the names vector.
+#' Note that the row dimension of the matrix has to be equal to the length of the
+#' names vector. The function was written to enable a rownames replacement in diplyr's pipe.
+#'
+#' @param matrix any matrix of dimension n*m
+#' @param newnames a character vector of length n
+#'
+#' @return
+#' @export
+#'
+#' @examples
+chg_rownames <- function(matrix, newnames){
+  rownames(matrix) <- newnames
+  return(matrix)
+}
